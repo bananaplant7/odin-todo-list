26e02ea5eb3fb7bf3585bae726f501403ee1b43e
bananaplant7 <brooklyncheng2002@gmail.com>
2022-05-25 19:17:27 -0700

    add todo container, header & sidebar w/ styling & some placeholder

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('yo')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi10b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3lvJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
